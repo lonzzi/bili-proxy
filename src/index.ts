56f1c6b4e6777eb41355d09fad93df1af55a4eee
lonzzi <lonzzi@qq.com>
2024-01-09 18:15:32 +0800

    feat: add bili proxy

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,67 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+const handler: ExportedHandler = {
+	async fetch(request) {
+		const corsHeaders = {
+			'Access-Control-Allow-Origin': '*',
+			'Access-Control-Allow-Methods': 'GET,HEAD,POST,OPTIONS',
+			'Access-Control-Max-Age': '86400',
+		};
 
-import handleProxy from './proxy';
-import handleRedirect from './redirect';
-import apiRouter from './router';
+		let API_URL = '';
 
-// Export a default object containing event handlers
-export default {
-	// The fetch handler is invoked when this worker receives a HTTP(S) request
-	// and should return a Response (optionally wrapped in a Promise)
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		// You'll find it helpful to parse the request.url string into a URL object. Learn more at https://developer.mozilla.org/en-US/docs/Web/API/URL
-		const url = new URL(request.url);
+		const apiUrlObj = new URL(request.url);
+		const apiUrl = request.url.replace(apiUrlObj.origin, '').slice(1);
 
-		// You can get pretty far with simple logic like if/switch-statements
-		switch (url.pathname) {
-			case '/redirect':
-				return handleRedirect.fetch(request, env, ctx);
-
-			case '/proxy':
-				return handleProxy.fetch(request, env, ctx);
+		if (apiUrl) {
+			API_URL = apiUrl;
 		}
 
-		if (url.pathname.startsWith('/api/')) {
-			// You can also use more robust routing
-			return apiRouter.handle(request);
+		if (apiUrl.startsWith('http:/') && !apiUrl.startsWith('http://')) {
+			API_URL = apiUrl.replace('http:/', 'http://');
+		} else if (apiUrl.startsWith('https:/') && !apiUrl.startsWith('https://')) {
+			API_URL = apiUrl.replace('https:/', 'https://');
 		}
 
-		return new Response(
-			`Try making requests to:
-      <ul>
-      <li><code><a href="/redirect?redirectUrl=https://example.com/">/redirect?redirectUrl=https://example.com/</a></code>,</li>
-      <li><code><a href="/proxy?modify&proxyUrl=https://example.com/">/proxy?modify&proxyUrl=https://example.com/</a></code>, or</li>
-      <li><code><a href="/api/todos">/api/todos</a></code></li>`,
-			{ headers: { 'Content-Type': 'text/html' } }
-		);
+		// console.log(API_URL);
+
+		const DEMO_PAGE = `
+		<!DOCTYPE html>
+		<html>
+		<body>
+			<h1>Bili Proxy</h1>
+		</body>
+		</html>
+	`;
+
+		const handleRequest = async (url: string) => {
+			const headers = {
+				'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36',
+				Referer: 'https://www.bilibili.com',
+			};
+
+			request = new Request(url, {
+				method: request.method,
+				headers,
+			});
+
+			let response = await fetch(request);
+			response = new Response(response.body, response);
+
+			response.headers.set('Access-Control-Allow-Origin', '*');
+			response.headers.set('Referrer-Policy', 'no-referrer');
+
+			return response;
+		};
+
+		if (API_URL && API_URL !== 'favicon.ico') {
+			return handleRequest(API_URL);
+		} else {
+			return new Response(DEMO_PAGE, {
+				headers: {
+					'content-type': 'text/html;charset=UTF-8',
+				},
+			});
+		}
 	},
 };
+
+export default handler;
